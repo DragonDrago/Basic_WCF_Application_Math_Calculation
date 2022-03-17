0a0bad7629dcde657b8d2cbf4e9843411be78585
DragonDrago <akobirmuso@gmail.com>
2022-03-17 18:49:02 +0500

    Add project files.

---
+++ WinformsClient/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformsClient
+{
+    public partial class Form1 : Form
+    {
+        MathServiceReference.MathServiceClient obj = new MathServiceReference.MathServiceClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MathServiceReference.MyNumbers obj1 = new MathServiceReference.MyNumbers();
+            obj1.Number1 = Convert.ToInt32(textBox1.Text);
+            obj1.Number2 = Convert.ToInt32(textBox2.Text);
+            int c = obj.AddAsync(obj1).Result; 
+            textBox3.Text = c.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MathServiceReference.MyNumbers obj1 = new MathServiceReference.MyNumbers();
+            obj1.Number1 = Convert.ToInt32(textBox1.Text);
+            obj1.Number2 = Convert.ToInt32(textBox2.Text);
+            int c = obj.SubtractAsync(obj1).Result;
+            textBox3.Text = c.ToString();
+        }
+    }
+}
